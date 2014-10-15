ffbbd50e14cca7f3468e009643ac8b8e2c6be36c
Ben Marshall <ben.marshall@stratfor.com>
2014-10-15 08:43:23 -0500

    Clean-up from v1.5.0 update.

--- grunt/clean.js
+++ grunt/clean.js
@@ -1,3 +1,3 @@
 module.exports = {
-    compass: ["build/css-dev", "build/js-dev"]
+    compass: ["build/css-dev", "build/js-dev", "src", ".gitignore", "config.rb", "Gruntfile.js", "package.json", "README.md", "grunt", "node_modules"]
 };
