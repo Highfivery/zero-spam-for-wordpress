03cca4b36193328c0c00d731ac7e3ae13694a359
Ben Marshall <me@benmarshall.me>
2014-10-21 19:10:27 -0500

    Cleanup

--- grunt/shell.js
+++ grunt/shell.js
@@ -6,7 +6,17 @@ module.exports = {
             'git fetch'
         ].join('&&')
     },
+    clean: {
+    	command: [
+    		'rm -rf build/js-dev/*',
+    		'rm -rf build/css-dev/*',
+    		'rm -rf build/img-dev/*',
+    		'rm -rf build/js/*',
+    		'rm -rf build/css/*',
+    		'rm -rf build/img/*'
+    	].join('&&')
+    },
     deploy: {
         command: './deploy.sh'
-    }
+    },
 };
