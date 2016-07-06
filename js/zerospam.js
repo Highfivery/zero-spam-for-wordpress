ab7b4419b4e42a0f91ae5d8a2427a1c6bef92a19
Ben Marshall <me@benmarshall.me>
2016-07-06 11:28:00 -0500

    Compiled scripts.

--- js/zerospam.js
+++ js/zerospam.js
@@ -1,1 +1,24 @@
-!function(a){"use strict";a(function(){var b="#commentform";b+=", #contactform",b+=", #registerform",b+=", .gform_wrapper form",b+=", #buddypress #signup_form",b+=", .zerospam",b+=", .ninja-forms-form",b+=", .wpforms-form","undefined"!=typeof zerospam.key&&(a(b).on("submit",function(){return a("<input>").attr("type","hidden").attr("name","zerospam_key").attr("value",zerospam.key).appendTo(b),!0}),a(".wpcf7-submit").click(function(){a("<input>").attr("type","hidden").attr("name","zerospam_key").attr("value",zerospam.key).appendTo(".wpcf7-form")}))})}(jQuery);
+(function($) {
+    "use strict";
+    $(function() {
+        var forms = "#commentform";
+        forms += ", #contactform";
+        forms += ", #registerform";
+        forms += ", #buddypress #signup_form";
+        forms += ", .zerospam";
+        forms += ", .ninja-forms-form";
+        forms += ", .wpforms-form";
+        if (typeof zerospam.key != "undefined") {
+            $(forms).on("submit", function() {
+                $("<input>").attr("type", "hidden").attr("name", "zerospam_key").attr("value", zerospam.key).appendTo(forms);
+                return true;
+            });
+            $(document).on("gform_post_render", function() {
+                $("<input>").attr("type", "hidden").attr("name", "zerospam_key").attr("value", zerospam.key).appendTo(".gform_wrapper form ");
+            });
+            $(".wpcf7-submit").click(function() {
+                $("<input>").attr("type", "hidden").attr("name", "zerospam_key").attr("value", zerospam.key).appendTo(".wpcf7-form");
+            });
+        }
+    });
+})(jQuery);
