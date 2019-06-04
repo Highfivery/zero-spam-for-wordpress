aea1701f2eeae04984f17779d132a5cadbe8f41f
Ben Marshall <bmarshall511@gmail.com>
2019-06-04 11:08:06 -0500

    Contributor comment updates

--- zero-spam.php
+++ zero-spam.php
@@ -9,7 +9,7 @@
  * License: GPL2
  */
 
-/*  Copyright 2015 - 2016  Ben Marshall  (email : me@benmarshall.me)
+/*  Copyright 2015 - 2019  Ben Marshall  (email : me@benmarshall.me)
 
 	This program is free software; you can redistribute it and/or modify
 	it under the terms of the GNU General Public License, version 2, as
